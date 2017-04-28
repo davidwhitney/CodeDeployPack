f83d630c54f3b414fcdc7ba1826f60e188f6d73a
David Whitney <david.whitney@thetrainline.com>
2017-04-28 14:04:27 +0100

    Removed unused refernces.

--- tests/CodeDeployPack.Test.Unit/AppSpecCreation/AppSpecGeneratorTests.cs
+++ tests/CodeDeployPack.Test.Unit/AppSpecCreation/AppSpecGeneratorTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using CodeDeployPack.AppSpecCreation;
 using CodeDeployPack.Test.Unit.TestDoubles;
 using NUnit.Framework;
